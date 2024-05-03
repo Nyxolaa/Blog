2fca64d20c6d1efa269b4016d75f01d5c1ef5598
Nyxola <88090162+Nyxolaa@users.noreply.github.com>
2024-05-04 00:21:28 +0300

    +

--- blog-backend/blog-backend/obj/Debug/net8.0/blog-backend.AssemblyInfo.cs
+++ blog-backend/blog-backend/obj/Debug/net8.0/blog-backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blog-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+865dbfbf4921cdcb6362a130665c57d3e7f24c35")]
 [assembly: System.Reflection.AssemblyProductAttribute("blog-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blog-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
